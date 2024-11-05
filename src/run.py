57713efaa331dd80a2a89143b00ee5b1e561757c
BeNNeTTcik <mateuszdampc99@gmail.com>
2024-11-05 19:32:18 +0100

    Gotowe

--- src/run.py
+++ src/run.py
@@ -5,4 +5,5 @@ import smote
 ds = "D:/ml/xgboost-main/data/ssh_logs/SSH.csv"
 
 dataset.df(ds)  # dataset create df on output
-smote.smote(X, y)  # create SMOTE
+
+smote.smote(X_train, y_train)  # create SMOTE
